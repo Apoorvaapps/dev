a219b17791b01fbbc5a78dc8dc851c988f224601
Apoorva Singh <Apoorva.Singh@nectechnologies.in>
2023-07-16 04:00:48 +0530

    main

--- project/main.tf
+++ project/main.tf
@@ -37,7 +37,7 @@ resource "azurerm_subnet" "my_terraform_subnet" {
 
 # Create public IPs
 resource "azurerm_public_ip" "my_terraform_public_ip" {
-  count               = var.instance_count == 1 ? 1 : 0
+  count               = var.instance_count == 2 ? 1 : 0
   name                = "myPublicIP-${count.index + 1}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
@@ -46,14 +46,14 @@ resource "azurerm_public_ip" "my_terraform_public_ip" {
 
 # Create network interface
 resource "azurerm_network_interface" "my_terraform_nic" {
-  count               = var.instance_count == 1 ? 1 : 0
+  count               = var.instance_count == 2 ? 1 : 0
   name                = "myNIC-${count.index + 1}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
     name                          = "my_nic_configuration"
-    subnet_id                     = azurerm_subnet.my_terraform_subnet.id
+    subnet_id                     = azurerm_subnet.my_terraform_subnet[count_index].id
     private_ip_address_allocation = "Dynamic"
     #public_ip_address_id          = azurerm_public_ip.my_terraform_public_ip.id
   }
