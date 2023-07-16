a88e15be676e9f41720286a6322c8677334a4e19
Apoorva Singh <Apoorva.Singh@nectechnologies.in>
2023-07-16 19:52:28 +0530

    added main.tf

--- project/main.tf
+++ project/main.tf
@@ -64,7 +64,7 @@ resource "azurerm_linux_virtual_machine" "my_terraform_vm" {
   name                  = "myVM-${count.index + 1}"
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
-  network_interface_ids = azurerm_network_interface.my_terraform_nic[count.index].id
+  network_interface_ids = [azurerm_network_interface.my_terraform_nic[count.index].id,]
   #network_interface_ids = "${element(azurerm_network_interface.my_terraform_nic.id, count.index)}"
   size                  = "Standard_DS1_v2"
 
